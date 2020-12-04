0bffb1545ce5308a8ab41e28158ddfdb3405f92b
bjornpiltz <bjornpiltz@gmail.com>
2020-12-04 22:19:59 +0100

    Update gmob.r

--- gmob.r
+++ gmob.r
@@ -2,26 +2,25 @@ library(tidyverse)
 library(readr)
 library(plotly)
 library(zoo)
-#library(extrafont)
+library(extrafont)
 #loadfonts(device = "win")
+library(tidyverse)
+countries <- c("Sweden", "Germany", "Denmark", "Belgium", "France", "Czechia")
+
+cols <- c("country", "Date", "Retail & recreation",
+          "Grocery & pharmacy",
+          "Parks", 
+          "Transit stations", 
+          "Workplaces",
+          "Residential_change")
 
 #download.file("https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv", "data/Global_Mobility_Report.csv")
-
 Global_Mobility_Report <- read_csv("data/Global_Mobility_Report.csv", 
                                    col_types = cols(census_fips_code = col_skip(), 
                                                     country_region_code = col_skip(), 
                                                     iso_3166_2_code = col_skip()))
 
-countries <- c("Sweden", "Belgium", "France", "Germany", "Czechia", "Denmark")
 
-cols <- c("country", "Date", "Retail & recreation",
-             "Grocery & pharmacy",
-             "Parks", 
-             "Transit stations", 
-             "Workplaces",
-             "Residential_change")
-
-colors <- c("#AF707070", "#7070AF70", "#AF70AF70", "#AFAF7070", "#70AFAF70", "#00AF00")
 
 All_Mobility_Data <- Global_Mobility_Report %>% 
   dplyr::select(- sub_region_2) %>%
@@ -31,24 +30,24 @@ All_Mobility_Data <- Global_Mobility_Report %>%
 
 colnames(All_Mobility_Data) <- cols
 
-All_Mobility_Data_long <- All_Mobility_Data %>% 
-  gather(Mobility_Type, Percent_Change, -c(Date, country))
-
-
-Subset_Mobility_Data_long <- All_Mobility_Data_long %>% 
+Subset_Mobility_Data_long <- All_Mobility_Data %>% 
+  gather(Mobility_Type, Percent_Change, -c(Date, country)) %>% 
   filter(country %in% countries) 
 
-g <- ggplot(Subset_Mobility_Data_long %>% filter(country == "Sweden") ,
+write_csv(Subset_Mobility_Data_long, "data/Subset_Mobility_Data_long.csv")
+
+Subset_Mobility_Data_long <- read_csv("data/Subset_Mobility_Data_long.csv")
+  
+g <- ggplot(Subset_Mobility_Data_long %>% 
+              filter(country == "Sweden")%>%
+              filter(Mobility_Type != "Parks") ,
             aes(x = Date, 
                 y = rollmean(Percent_Change, 7, na.pad=TRUE, align = "right"), 
                 color = Mobility_Type)) + 
   geom_line() + 
   theme_light() +
   ggtitle("Google mobility report - Sweden") +
-  ylab("Change from baseline") +
-  ylim(-75, 25) 
-#  geom_vline(xintercept=as.numeric(black_friday), linetype="dashed", color = "black") +
-#  annotate("text", x=black_friday, y=-75, label="Black friday ", hjust = "right")
+  ylab("Change from baseline") 
 
 ggplotly(g)%>% style(textposition = "left")
 
@@ -64,19 +63,11 @@ g <- ggplot(All_Mobility_Data_long %>% filter(country == "Germany") ,
   ggtitle("Google mobility report - Germany") +
   ylab("Change from baseline") +
   ylim(-75, 25) 
-#  geom_vline(xintercept=as.numeric(black_friday), linetype="dashed", color = "black") +
-#  annotate("text", x=black_friday, y=-75, label="Black friday ", hjust = "right")
+  #geom_vline(xintercept=as.numeric(black_friday), linetype="dashed", color = "black") +
+  #annotate("text", x=black_friday, y=-75, label="Black friday ", hjust = "right")
   
 ggplotly(g)%>% style(textposition = "left")
 g
-
-
-
-
-
-
-
-
 
 
 #library(showtext)
@@ -88,66 +79,75 @@ g
 #library(extrafont)
 #font_import()
 #loadfonts(device = "win")
+our_theme <- theme_minimal() + 
+  theme(text=element_text(family="ETBembo"))
+
+colors <- c("#AF707070", "#7070AF70", "#AF70AF70", "#AFAF7070", "#70AFAF70", "#00AF00")
+
+plot_mob_cat <- function (Mob_Type)  
+{
+  ggplot(Subset_Mobility_Data_long %>% filter(Mobility_Type == Mob_Type),
+         aes(Date, rollmean(Percent_Change, 7, na.pad=TRUE, align = "right"),
+             color = country)) + 
+    geom_line() +
+    theme_light() +
+    theme(text=element_text(family="ETBembo")) +
+    ggtitle(paste("Google mobility report - ", Mob_Type)) +
+    ylab("Change from baseline") +
+    scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+    scale_color_manual(name = "Country", values = colors)+
+    geom_hline(yintercept=0, linetype="dashed") 
+}
 
 
-#  R n R
-g <- ggplot(Subset_Mobility_Data_long %>% filter(Mobility_Type == "Retail & recreation"),
-          aes(Date, rollmean(Percent_Change, 7, na.pad=TRUE, align = "right"),
-              color = country)) + 
-  geom_line() +
-  theme_light() +
-  theme(text=element_text(family="ETBembo")) +
-  ggtitle("Google mobility report - Retail & recreation") +
-  ylab("Change from baseline") +
-  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
-  scale_color_manual(name = "Country", values = colors)
+ggplotly(plot_mob_cat("Residential_change"))
+plot_mob_cat("Residential_change")
+ggplotly(plot_mob_cat("Retail & recreation"))
+ggplotly(plot_mob_cat("Workplaces"))
+ggplotly(plot_mob_cat("Parks"))
+ggplotly(plot_mob_cat("Transit stations"))
 
-ggplotly(g)
-g
+plot_country <- function (Country)  
+{
+  g <- ggplot(All_Mobility_Data_long %>% 
+                filter(country == Country)%>%
+                filter(Mobility_Type != "Parks") , 
+              aes(x = Date, 
+                  y = rollmean(Percent_Change, 7, na.pad=TRUE, align = "right"), 
+                  color = Mobility_Type)) + 
+    geom_line() + 
+    theme_light() +
+    theme(text=element_text(family="ETBembo")) +
+    ggtitle(paste("Google mobility report - ", Country)) +
+    ylab("Change from baseline") 
+}
 
+countries <- c("Sweden", "Germany", "Denmark", "Belgium", "France", "Czechia")
+ggplotly(plot_country("Sweden"))
+ggplotly(plot_country("Germany"))
+ggplotly(plot_country("Denmark"))
+ggplotly(plot_country("Belgium"))
+ggplotly(plot_country("France"))
+ggplotly(plot_country("Czechia"))
 
-#  Work
-g <- ggplot(Subset_Mobility_Data_long %>% filter(Mobility_Type == "Workplaces"),
+plot_mob_facet <- function (Mob_Type)  
+{
+  g<-ggplot(transform(Subset_Mobility_Data_long,country=factor(country,levels=countries)) %>% 
+              filter(Mobility_Type == Mob_Type),
             aes(Date, rollmean(Percent_Change, 7, na.pad=TRUE, align = "right"),
                 color = country)) + 
-  geom_line() +
-  theme_light() +
-  theme(text=element_text(family="ETBembo")) +
-  ggtitle("Google mobility report - Workplaces") +
-  ylab("Change from baseline") +
-  scale_color_manual(name = "Country", values = colors)
+    geom_line() +
+    our_theme +
+    ggtitle(paste("Google mobility report - ", Mob_Type)) +
+    ylab("Change from baseline") +
+    scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+    geom_hline(yintercept=0, linetype="dashed")  +
+    scale_x_date(date_breaks = "1 month", limits = as.Date(c('2020-10-01','2020-12-02'))) +
+    facet_wrap(~ country)
+}
 
-ggplotly(g)
-g
-
-#  Parks
-g <- ggplot(Subset_Mobility_Data_long %>% filter(Mobility_Type == "Parks"),
-            aes(Date, rollmean(Percent_Change, 7, na.pad=TRUE, align = "right"),
-                color = country)) + 
-  geom_line() +
-  theme_light() +
-  theme(text=element_text(family="ETBembo")) +
-  ggtitle("Google mobility report - Parks") +
-  ylab("Change from baseline") +
-  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
-  scale_color_manual(name = "Country", values = colors)
-
-ggplotly(g)
-g
-
-#  Transit stations
-g <- ggplot(Subset_Mobility_Data_long %>% filter(Mobility_Type == "Transit stations"),
-            aes(Date, rollmean(Percent_Change, 7, na.pad=TRUE, align = "right"),
-                color = country)) + 
-  geom_line() +
-#  geom_smooth(method="auto" , se=TRUE, span = 0.3) +
-#geom_smooth(method="auto" , se=TRUE, span = 0.1, aes(y=Percent_Change, linetype="None")) +
-  theme_light() +
-  theme(text=element_text(family="ETBembo")) +
-  ggtitle("Google mobility report - Transit stations") +
-  ylab("Change from baseline") +
-  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
-  scale_color_manual(name = "Country", values = colors)
-
-ggplotly(g)
-g
+ggplotly(plot_mob_facet("Residential_change"))  %>% style(hoverinfo = "none")
+ggplotly(plot_mob_facet("Retail & recreation"))
+ggplotly(plot_mob_facet("Workplaces"))
+ggplotly(plot_mob_facet("Parks"))
+ggplotly(plot_mob_facet("Transit stations"))
